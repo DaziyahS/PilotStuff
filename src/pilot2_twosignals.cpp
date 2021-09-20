a6ce33cf9741ea786fe8587d07579628dce41bfd
Daziyah Sullivan <dhs4@rice.edu>
2021-09-20 16:15:24 -0500

    lots of things for logging with csv

--- src/pilot2_twosignals.cpp
+++ src/pilot2_twosignals.cpp
@@ -1,17 +1,21 @@
 #include <Mahi/Gui.hpp>
 #include <Mahi/Util.hpp>
 #include <Mahi/Util/Logging/Log.hpp>
+#include <Mahi/Util/Timing/Timestamp.hpp>
 #include <syntacts>
 #include <random>
 #include <iostream> 
 #include <fstream> // need to include inorder to save to csv
 #include <chrono> 
 #include <string> // for manipulating file name
+#include <array> // for csv use
+
+#include <sys/timeb.h>
+#include <time.h>
 
 // local includes
 #include <Chord.hpp>
 #include <Note.hpp> // would this imply that the .cpp functionality is attached?
-// #include <notes_info.hpp> // probably do not need
 
 // open the namespaces that are relevant for this code
 using namespace mahi::gui;
@@ -43,23 +47,28 @@ public:
     // this is a constructor. It initializes your class to a specific state
     MyGui() : 
     Application(windowWidth, windowHeight, my_title, 0),
-    chordNew(),
-    channelSignals(3)
+    chordNew1(),
+    chordNew2(),
+    channelSignals(3),
+    data(11)
     {
         s.open(deviceNdx); // , tact::API::MME); // opens session with the application
         // keep in mind, if use device name must also use the API
         // something the GUI needs *shrugs*
         ImGui::GetIO().ConfigFlags &= ~ImGuiConfigFlags_ViewportsEnable;
         set_background(Cyans::Teal); //background_color = Grays::Black; 
+        // anything that does things should be in the constructor (AKA here)
+        std::string filepath = "../../Data/" + wherePut.yyyy_mm_dd_hh_mm_ss();
+        mahi::util::csv_write_row(filepath, headers);
      }
 
     // Define variables needed throughout the program
     // For creating the signal
     std::string defaultName = "a_minor_n1"; // what is the first chord name
-    std::string currentChord; // holds name of current chord based on selection
-    Chord chordNew;
+    std::string currentChord1, currentChord2; // holds name of current chord based on selection
+    Chord chordNew1, chordNew2;
     std::vector<tact::Signal> channelSignals;
-    bool isSim = false; // default is sequential
+    std::vector<bool> isSim = {false, false}; // default is sequential
     // For saving the signal
     std::string sigName; // name for saved signal
     std::string fileLocal; // for storing the signal
@@ -71,6 +80,12 @@ public:
     bool play_once = false;    // for playing a cue one time
     bool start_loop = false;  // for playing a cue multiple times
     int pause = 1; // can pause the cue at any time
+    // For logging the data
+    mahi::util::Timestamp wherePut;
+    std::vector<std::string> headers = {"Trial", "Sus 1", "Amp 1", "Chord 1", "IsSim 1", "Sus 2", "Amp 2", "Chord 2","IsSim 2", 
+                                        "Valence", "Arousal"};   
+    std::vector<int> data; 
+    std::string filepath;
 
     virtual void update() override
     {
@@ -80,32 +95,50 @@ public:
         const char* items[] = { "A Minor 1", "A Major 1", "B Minor 1", "B Major 1", "C Minor 2", "C Major 2", 
                                 "D Minor 2", "D Major 2", "E Minor 2", "E Major 2", "F Minor 2", "F Major 2",
                                 "G Minor 2", "G Major 2"}; // chord names
-        static int item_current = 0; // Starts at first item in the list
-        const char* combo_label = items[item_current]; // Gives the preview before anything is played
-        if(ImGui::BeginCombo("Presets", combo_label)){ // no flags
+        static int item_current1 = 0; // Starts at first item in the list
+        const char* combo_label1 = items[item_current1]; // Gives the preview before anything is played
+        if(ImGui::BeginCombo("Note 1 Chord", combo_label1)){ // no flags
             for (int n = 0; n < IM_ARRAYSIZE(items); n++)
             {
-                const bool is_selected = (item_current == n);
+                const bool is_selected = (item_current1 == n);
                 if (ImGui::Selectable(items[n], is_selected))
-                    item_current = n; // gives a value to each selection when intialized
+                    item_current1 = n; // gives a value to each selection when intialized
                 if (is_selected)
-                    ImGui::SetItemDefaultFocus(); // focuses on item selected, item_current is set as selected
+                    ImGui::SetItemDefaultFocus(); // focuses on item selected, item_current1 is set as selected
             }
 
             // determine the current signal
-            currentChord = chordNew.signal_list[item_current]; // theoretically gives name needed
+            currentChord1 = chordNew1.signal_list[item_current1]; // theoretically gives name needed
+
+            ImGui::EndCombo();
+        };
+        ImGui::SameLine();
+        static int item_current2 = 0; // Starts at first item in the list
+        const char* combo_label2 = items[item_current2]; // Gives the preview before anything is played
+        if(ImGui::BeginCombo("Note 2 Chord", combo_label2)){ // no flags
+            for (int n = 0; n < IM_ARRAYSIZE(items); n++)
+            {
+                const bool is_selected = (item_current2 == n);
+                if (ImGui::Selectable(items[n], is_selected))
+                    item_current2 = n; // gives a value to each selection when intialized
+                if (is_selected)
+                    ImGui::SetItemDefaultFocus(); // focuses on item selected, item_current2 is set as selected
+            }
+
+            // determine the current signal
+            currentChord2 = chordNew2.signal_list[item_current2]; // theoretically gives name needed
 
             ImGui::EndCombo();
         };
 
         // for sustain and delay
-        static int sus = 0; // I think this is the vector being adjusted
-        if(ImGui::SliderInt("Sustain", &sus, 0, 2)){  // if use SliderInt2 will have 2 back to back same range
+        static int sus [2] = {0, 0}; // this is the vector being adjusted
+        if(ImGui::SliderInt("Sustain", sus, 0, 2)){  // if use SliderInt2 will have 2 back to back same range
             // sus is determined here, this is duration value
             std::cout << "sustain is " << sus << std::endl;
         }; 
-        static int amp = 0; // The value to be adjusted
-        if(ImGui::SliderInt("Intensity", &amp, 0, 3)){
+        static int amp [2] = {0, 0}; // The value to be adjusted
+        if(ImGui::SliderInt("Intensity", amp, 0, 3)){
             // amp is determined here, this is amplitude value
         };
         /* // if I just want it to be an arrow down and would go before it
@@ -113,17 +146,25 @@ public:
                 flags &= ~ImGuiComboFlags_NoArrowButton; // Clear the other flag, as we cannot combine both
         */
         if(ImGui::Button("Sequential", buttonSize)){
-            isSim = 0; // false
+            isSim[0] = 0; // false
         };
         ImGui::SameLine();
         if(ImGui::Button("Simultaneous", buttonSize)){
-            isSim = 1; // true
+            isSim[0] = 1; // true
+        };
+        ImGui::SameLine();
+        if(ImGui::Button("Sequential", buttonSize)){
+            isSim[1] = 0; // false
+        };
+        ImGui::SameLine();
+        if(ImGui::Button("Simultaneous", buttonSize)){
+            isSim[1] = 1; // true
         };
 
         // for play, loop, pause, save
-        if(ImGui::Button("Play", buttonSize)){
-            chordNew = Chord(currentChord, sus, amp, isSim);
-            channelSignals = chordNew.playValues(); // get the values of the signal
+        if(ImGui::Button("Play Note 1", buttonSize)){
+            chordNew1 = Chord(currentChord1, sus[0], amp[0], isSim[0]);
+            channelSignals = chordNew1.playValues(); // get the values of the signal
 
             // replace the loop
             pause = 0;
@@ -142,27 +183,9 @@ public:
             std::cout << channelSignals[0].length() << " s for playing" << std::endl;
             }; 
         ImGui::SameLine();
-        if(ImGui::Button("Loop", buttonSize)){
-            // Initialize the ability to repeat
-            pause = 0;
-
-            // determine the signal
-            chordNew = Chord(currentChord, sus, amp, isSim);
-            channelSignals = chordNew.playValues(); // get the values of the signal
-
-            std::cout << "Pause value is " << pause << std::endl;
-            std::cout << channelSignals[0].length() << " s for playing" << std::endl;
-            start_loop = true;
-        }; 
-        ImGui::SameLine();        
-        if(ImGui::Button("Pause", buttonSize)){
-                pause = 1;
-            }; 
-        /*
-        if(ImGui::Button("Reverse", buttonSize)){ // You may want to create a popup with repeat option
-            chordNew(currentChord, sus, amp, isSim);
-            channelSignals = chordNew.playValues(); // get the values of the signal
-
+        if(ImGui::Button("Play Note 2", buttonSize)){
+            chordNew2 = Chord(currentChord2, sus[1], amp[1], isSim[1]);
+            channelSignals = chordNew2.playValues(); // get the values of the signal
 
             // replace the loop
             pause = 0;
@@ -172,12 +195,18 @@ public:
             play_clock.restart();
 
             // Play the signal
-            s.play(1, channelSignals[0]); // play has (channel, signal)
-            s.play(2, channelSignals[1]); 
-            s.play(3, channelSignals[2]); 
+            s.play(leftTact, channelSignals[0]); // play has (channel, signal)
+            s.play(botTact, channelSignals[1]); 
+            s.play(rightTact, channelSignals[2]); 
             // sleep(finalSignal.length()); // sleep makes sure you cannot play another cue before that cue is done (in theory)
             // sleep is a blocking function
-        }; */
+
+            std::cout << channelSignals[0].length() << " s for playing" << std::endl;
+            }; 
+      
+        if(ImGui::Button("Pause", buttonSize)){
+                pause = 1;
+            }; 
 
         // Stop the signal with pause loop
         if (pause == 1 || !start_loop){
@@ -209,42 +238,6 @@ public:
             play_clock.restart(); // attempting a non-blocking version of sleep, reset the counter
         }
 
-        if(ImGui::Button("Save", buttonSize))
-        {
-            ImGui::OpenPopup("saving_things"); // open a popup and name it for calling
-            // This just needs its own space, no curlies for the if
-        }   
-        // necessary variables
-        static char buf[31]; // name holder
-        if(ImGui::BeginPopup("saving_things")) // if clicked essentially
-        {
-            ImGui::Text("Signal name is: "); // precursor for people to understand
-            ImGui::InputText("##edit", buf, IM_ARRAYSIZE(buf), ImGuiInputTextFlags_CharsNoBlank); // no space allowed, size wanted
-            if (ImGui::Button("Close"))
-            {
-                if (isSim == false){
-                ImGui::CloseCurrentPopup();
-                // put things here for what should happen once closed or else it will run foreverrrr
-                std::string predone(buf); // gets rid of null characters
-                sigName = predone + ".sig"; // now set the name to what we want
-                std::cout << sigName << std::endl;
-
-                // determine the signal
-                chordNew = Chord(currentChord, sus, amp, isSim);
-                channelSignals = chordNew.playValues(); // get the values of the signal
-
-
-                // Save the signal
-                fileLocal = "../../Library/" + sigName; // create file path for library
-                tact::Library::exportSignal(channelSignals[0], fileLocal); // export signal to library
-                }
-                else{
-                    // do nothing because cannot because simultaneous and all different
-                }
-            }
-            ImGui::EndPopup();            
-        }
-        ImGui::SameLine();
         if(ImGui::Button("Log", buttonSize))
         {
             ImGui::OpenPopup("logging_things"); // open a popup and name it for calling
@@ -263,8 +256,9 @@ public:
                 ImGui::CloseCurrentPopup();
                 // put things here for what should happen once closed or else it will run foreverrrr
                 std::string notes_taken(num); // gets rid of null characters
-                // LOG(Info) << "Notes for trial " << trial_num << " are: " + notes_taken + " for the " << item_current << " chord with a hold of " << sus << " and amplitude of " << amp << "."; // now log this information
-                LOG(Info) << trial_num << "," << item_current << "," << sus << "," << amp << "," << val << "," << arous << "," + notes_taken;
+                data = {trial_num, sus[0], amp[0], item_current1, isSim[0], sus[1], amp[1], item_current2, isSim[1], val, arous};
+                csv_append_rows(filepath, data);
+                LOG(Info) << notes_taken;
                 trial_num++;
             }
             ImGui::EndPopup();            
