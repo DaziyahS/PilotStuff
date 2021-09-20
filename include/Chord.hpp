0bc1dc7954584cd55c2da31b118b088bd60e2d3e
Daziyah Sullivan <dhs4@rice.edu>
2021-09-19 22:39:48 -0500

    added major boolean

--- include/Chord.hpp
+++ include/Chord.hpp
@@ -18,21 +18,22 @@ struct Chord
         std::vector<int> notes_;
         std::string name_; // name of the note
         bool isSimultaneous_; // holds info of which type of sequence
+        bool isMajor_; // if 1 then it is major
     private:
-        std::vector<int> a_minor_n1 = {1, 5, 9};
-        std::vector<int> a_major_n1 = {1, 6, 9};
-        std::vector<int> b_minor_n1 = {3, 8, 11};
-        std::vector<int> b_major_n1 = {3, 7, 11};
+        // define the chords based on numbers and whether it is major
+        std::vector<int> a_minor_n1 = {1, 5, 9, 0};
+        std::vector<int> a_major_n1 = {1, 6, 9, 1};
+        std::vector<int> b_minor_n1 = {3, 8, 11, 0};
+        std::vector<int> b_major_n1 = {3, 7, 11, 1};
 
-        std::vector<int> c_minor_n2 = {5, 8, 12};
-        std::vector<int> c_major_n2 = {5, 9, 12};
-        std::vector<int> d_minor_n2 = {7, 12, 14};
-        std::vector<int> d_major_n2 = {7, 11, 14};
-        std::vector<int> e_minor_n2 = {9, 12, 16};
-        std::vector<int> e_major_n2 = {9, 13, 16};
-        std::vector<int> f_minor_n2 = {10, 13, 18};
-        std::vector<int> f_major_n2 = {10, 14, 18};
-        std::vector<int> g_minor_n2 = {12, 15, 20};
-        std::vector<int> g_major_n2 = {12, 16, 20};
-
+        std::vector<int> c_minor_n2 = {5, 8, 12, 0};
+        std::vector<int> c_major_n2 = {5, 9, 12, 1};
+        std::vector<int> d_minor_n2 = {7, 12, 14, 0};
+        std::vector<int> d_major_n2 = {7, 11, 14, 1};
+        std::vector<int> e_minor_n2 = {9, 12, 16, 0};
+        std::vector<int> e_major_n2 = {9, 13, 16, 1};
+        std::vector<int> f_minor_n2 = {10, 13, 18, 0};
+        std::vector<int> f_major_n2 = {10, 14, 18, 1};
+        std::vector<int> g_minor_n2 = {12, 15, 20, 0};
+        std::vector<int> g_major_n2 = {12, 16, 20, 1};
 };
